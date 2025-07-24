8fe7b4c83e092423617b6c79352af7693e7d8479
Vimukthi <vimukthikaushalya2114@gmail.com>
2025-07-24 15:40:45 +0530

    Updated all

--- app/ffmpeg_worker.py
+++ app/ffmpeg_worker.py
@@ -87,46 +87,46 @@ class FFmpegWorker:
         pixels = width * height
         
         # Target: 120MB for 10 minutes = 12MB/min = 1.6Mbps average
-        # VBR settings optimized by resolution (scaled from 1080p: 1500k avg, 2000k max)
+        # VBR settings optimized by resolution (scaled from 1080p: 1000k avg, 1500k max)
         if pixels <= 640 * 360:  # 360p and below
             return {
-                'avg_bitrate': '400k',
-                'max_bitrate': '600k', 
+                'avg_bitrate': '300k',
+                'max_bitrate': '450k', 
                 'crf': 30,
                 'preset': 'medium'
             }
         elif pixels <= 720 * 480:  # SD (480p)
             return {
-                'avg_bitrate': '600k',
-                'max_bitrate': '800k', 
+                'avg_bitrate': '450k',
+                'max_bitrate': '650k', 
                 'crf': 28,
                 'preset': 'medium'
             }
         elif pixels <= 1280 * 720:  # HD (720p)
             return {
-                'avg_bitrate': '900k',
-                'max_bitrate': '1200k',
+                'avg_bitrate': '700k',
+                'max_bitrate': '1000k',
                 'crf': 26,
                 'preset': 'medium'
             }
         elif pixels <= 1920 * 1080:  # Full HD (1080p)
             return {
-                'avg_bitrate': '1500k',
-                'max_bitrate': '2000k',
+                'avg_bitrate': '1000k',
+                'max_bitrate': '1500k',
                 'crf': 24,
                 'preset': 'medium'
             }
         elif pixels <= 2560 * 1440:  # QHD (1440p)
             return {
-                'avg_bitrate': '2200k',
-                'max_bitrate': '3000k',
+                'avg_bitrate': '1600k',
+                'max_bitrate': '2400k',
                 'crf': 22,
                 'preset': 'slow'
             }
         else:  # 4K and above
             return {
-                'avg_bitrate': '3500k',
-                'max_bitrate': '4500k',
+                'avg_bitrate': '2500k',
+                'max_bitrate': '3500k',
                 'crf': 20,
                 'preset': 'slow'
             }
